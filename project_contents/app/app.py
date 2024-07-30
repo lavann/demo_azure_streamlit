28acbdce61eba322efab4ebea4eed3a95194aa7f
lavann <lavann@gmail.com>
2024-07-30 13:03:53 +0100

    Update app.py
    commit for workshop

--- project_contents/app/app.py
+++ project_contents/app/app.py
@@ -5,3 +5,4 @@ import streamlit as st
 st.title("Hello World @ 29/07/2024 @ 13:30")
 st.text("This is some text.")
 st.text("This is some more text.")
+st.text("This is some more text - 30-07-2024.")
